8d19fedd3ff764da72891c323572a7b6c1c5a216
lh317 <46079517+lh317@users.noreply.github.com>
2025-12-05 19:35:03 -0500

    Day 4 Part 2

--- 04-paper/src/main.rs
+++ 04-paper/src/main.rs
@@ -1,7 +1,9 @@
-use std::cmp::{max, min};
-
 use eyre::{Context, OptionExt, Result, eyre};
-use ndarray::{s, Array2, ArrayView2};
+use ndarray::{Array2, ArrayRef2};
+
+fn in_bounds((y, x): (isize, isize), (rows, cols): (isize, isize)) -> bool {
+    y >= 0 && y < rows && x >= 0 && x < cols
+}
 
 fn parse_file(body: &str, file_name: Option<&str>) -> Result<Array2<bool>> {
     let mut rows = 0usize;
@@ -15,71 +17,71 @@ fn parse_file(body: &str, file_name: Option<&str>) -> Result<Array2<bool>> {
         if rows == 1 {
             columns = line.len();
         } else if line.len() != columns {
-            return Err(eyre!("{}:{rows}: line not of expected length {columns}", file_name.unwrap_or("unknown")));
+            return Err(eyre!(
+                "{}:{rows}: line not of expected length {columns}",
+                file_name.unwrap_or("unknown")
+            ));
         }
     }
     Array2::from_shape_vec((rows, columns), values).wrap_err("error creating array")
 }
 
-fn count_accessible(rolls: ArrayView2<bool>) -> usize {
-    let mut count = 0usize;
-    for ((y, x), is_roll) in rolls.indexed_iter() {
-        if !*is_roll {
-            continue;
-        }
-        let ul = if y >= 1 && x >= 1 {
-            rolls[[y-1, x-1]] as u8
-        } else {
-            0u8
-        };
-        let u = if y >= 1 {
-            rolls[[y-1, x]] as u8
-        } else {
-            0u8
-        };
-        let ur = if y >= 1 && x + 1 < rolls.ncols()  {
-            rolls[[y-1, x+1]] as u8
-        } else {
-            0u8
-        };
-        let l = if x >= 1 {
-            rolls[[y, x-1]] as u8
-        } else {
-            0u8
-        };
-        let r = if x + 1 < rolls.ncols() {
-            rolls[[y, x+1]] as u8
-        } else {
-            0u8
-        };
-        let bl = if y + 1 < rolls.nrows() && x >= 1 {
-            rolls[[y+1, x-1]] as u8
-        } else {
-            0u8
-        };
-        let b = if y + 1 < rolls.nrows() {
-            rolls[[y+1, x]] as u8
-        } else {
-            0u8
-        };
-        let br = if y + 1 < rolls.nrows() && x + 1 < rolls.ncols() {
-            rolls[[y+1, x+1]] as u8
-        } else {
-            0u8
-        };
-        if ul + u + ur + l + r + bl + b + br < 4 {
-            count += 1;
-        }
-    }
-    count
+fn is_accessible((y, x): (usize, usize), rolls: &ArrayRef2<bool>) -> bool {
+    let y = isize::try_from(y).expect("array too large");
+    let x = isize::try_from(x).expect("array too large");
+    let rows = isize::try_from(rolls.nrows()).expect("array too large");
+    let cols = isize::try_from(rolls.ncols()).expect("array too large");
+    let pos = [
+        (y - 1, x - 1),
+        (y - 1, x),
+        (y - 1, x + 1),
+        (y, x - 1),
+        (y, x + 1),
+        (y + 1, x - 1),
+        (y + 1, x),
+        (y + 1, x + 1),
+    ];
+    pos.iter()
+        .filter(|&&(y, x)| in_bounds((y, x), (rows, cols)) && rolls[[y as usize, x as usize]])
+        .count()
+        < 4
 }
-
 
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let fname = args.nth(1).ok_or_eyre("filename was not provided")?;
     let body: String = std::fs::read_to_string(&fname)?;
-    let rolls = parse_file(&body, Some(&fname))?;
-    println!("{}", count_accessible(rolls.view()));
-    Ok(())
+    let mut rolls = parse_file(&body, Some(&fname))?;
+    let mut removed = rolls
+        .indexed_iter()
+        .filter_map(|(pos, c)| {
+            if *c && is_accessible(pos, &rolls) {
+                Some(pos)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let mut count = removed.len();
+    println!("{}", count);
+    loop {
+        if removed.is_empty() {
+            println!("{count}");
+            return Ok(());
+        }
+        for pos in removed {
+            rolls[pos] = false;
+        }
+        removed = rolls
+            .indexed_iter()
+            .filter_map(|(pos, c)| {
+                if *c && is_accessible(pos, &rolls) {
+                    Some(pos)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        count += removed.len();
+    }
 }
